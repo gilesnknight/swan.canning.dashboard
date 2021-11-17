5bf3c7814637ca0a867c99d1e6337b47d1cdbb21
Giles Knight <62376886+gilesnknight@users.noreply.github.com>
2021-11-17 16:18:41 +0800

    simple air temperature plots

--- shiny/R/app.R
+++ shiny/R/app.R
@@ -7,69 +7,111 @@ library(magrittr)
 library(dplyr)
 library(ggplot2)
 library(thematic)
-
-#setwd('Documents/GitHub/swan.canning.dashboard/shiny/R/')
-
+library(patchwork)
 
 
 # Define UI
 ui <- navbarPage(
-    theme = bs_theme(bootswatch = "solar"),
+    theme = bs_theme(bootswatch = "minty"),
     title = "SCEVO",
-    sidebarPanel(
-        selectInput(
-            "site",
-            "Select site:",
-            c("Matilda")
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(
+                "site",
+                "Select site:",
+                c(
+                    "Bickley"="sensor_repository_00038",
+                    #"Champion Lakes"="sensor_repository_00143",
+                    "Dalwallinu"="sensor_repository_00199",
+                    "Dwellingup"="sensor_repository_00220",
+                    "Garden Island" = "sensor_repository_00248",
+                    "Gingin Airport" = "sensor_repository_00290",
+                    "Jandakot" = "sensor_repository_00360",
+                    "Lake Grace" = "sensor_repository_00416",
+                    "Manjimup" = "sensor_repository_00458",
+                    "Perth Airport" = "sensor_repository_00592",
+                    "Perth Metro" = "sensor_repository_00620",
+                    "Rottnest" = "sensor_repository_00648",
+                    "Southern Cross Airport" = "sensor_repository_00662",
+                    "Swanbourne" = "sensor_repository_00697",
+                    "Wandering" = "sensor_repository_00711",
+                    "Chunderin Airport" = "sensor_repository_00178"
+                )
+            ),
+            sliderInput("dateInput",
+                    "Dates:",
+                    min=as.Date("2020-01-01","%Y-%m-%d"),
+                    max=as.Date("2021-12-22","%Y-%m-%d"),
+                    value=c(as.Date("2020-01-01"), as.Date("2021-12-22")),
+                    timeFormat="%Y-%m-%d",
+                    width = '100%',
+                    animate = animationOptions(1000)
+        )
         ),
-        dateRangeInput('dateRange',
-                   label = 'Filter by date',
-                   start = as.Date("2021-10-15") , end = as.Date("2021-10-15")
-    ),
-        sliderInput("dateInput",
-                "Dates:",
-                min=as.Date("2016-08-29","%Y-%m-%d"),
-                max=as.Date("2020-12-22","%Y-%m-%d"),
-                value=c(as.Date("2016-08-29"), as.Date("2020-12-22")),
-                timeFormat="%Y-%m-%d",
-                width = '100%',
-                animate = animationOptions(1000)
-    )
-    ),
-    mainPanel(
-        plotOutput('plot')
+        mainPanel(
+            plotOutput('plot')
+        )
     )
 
 )
 
 # Define server function
-server <- function(input, output) {
-    thematic_shiny(font = "auto")
+server <- function(input, output, session) {
     drv <- dbDriver("PostgreSQL")
     conn <- dbConnect(
         drv, host='130.95.204.63', port='5433', dbname='scevo',
         user='dpaw_write', password='dpaw_write'
         )
-    sensor_data <-tbl(conn, "sensor_repository_01000") %>% as.data.frame()
-    #sensor_data  <- reactive(subset(sensor_data, datestamp> "2021-10-15" & datestamp "2021-10-15"))
-   # print(head(sensor_data))
-    output$plot  <- renderPlot({
-        #req(sensor_data())
-        sensor_data <-  dplyr::filter(sensor_data,datestamp >= as.Date(input$dateInput[1]),datestamp <= as.Date(input$dateInput[2]))
-        #sensor_data <- sensor_data %>% filter(datestamp >= input$dateRange[1] & datestamp <= input$dateRange[2])
-        ggplot(sensor_data, aes(x=datestamp, y=st_value_1)) +
-        geom_line() +
-        xlab("Time") +
-        ylab("Value")
+
+    observeEvent(input$site,{
+        sensor_data <- tbl(conn, input$site) %>% as.data.frame()
+
+        sensor_data$datetime  <- as.POSIXlt(sensor_data$st_feed_date_jdn*86400,
+                                    origin=structure(-210866760000,
+                                                     class=c("POSIXct", "POSIXt"),
+                                                     tzone="Australia/Perth"),
+                                    tz="Australia/Perth")
+        data  <- sensor_data
+
+        updateSliderInput(session, "dateInput", label = "Dates:",
+            min = min(as.Date(data$datetime)), max = max(as.Date(data$datetime)),
+            value = c(max(as.Date(data$datetime))-7, max(as.Date(data$datetime)))
+            )
+
+        output$plot  <- renderPlot({
+            data <-  dplyr::filter(data,datetime >= as.POSIXct(input$dateInput[1]),datetime <= as.POSIXct(input$dateInput[2]))
+            p1  <- ggplot(data, aes(x=as.POSIXct(datetime), y=st_value_1)) +
+            geom_line() +
+            xlab("Date") +
+            ylab("Air Temperature") +
+            theme_light(base_size = 14)
+            p2  <- ggplot(data=data,aes(x=1, y=st_value_1))+
+                    scale_y_continuous(position = "right") +
+                    geom_violin()+
+                    stat_summary(fun = "mean", geom = "point",
+                                colour = "black")+
+                    theme_light(base_size = 14)+
+                    theme(
+                        axis.text.x = element_blank(),
+                        axis.title.x = element_blank(),
+                        axis.ticks.x = element_blank(),
+                        axis.line.x = element_blank(),
+                        panel.grid.major.x = element_blank(),
+                        panel.grid.minor.x = element_blank(),
+                        axis.title.y = element_blank()
+                    )
+            p1+p2+plot_layout(widths = c(5, 1))
+        })
     })
-    observeEvent(input$startDate,(
-        print(input$startDate)
-    ))
+
+    # observeEvent(input$dateInput,(
+    #     print(input$dateInput)
+    # ))
 
 }
 
 # Create Shiny object
-thematic_shiny()
+
 shinyApp(ui = ui, server = server)
 
-#bootswatch_themes(version = version_default(), full_path = FALSE)
+
